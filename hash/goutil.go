dd0c5779317695085b62d52ea5a9710822146516
Adxie <jianxin.xie@advantech.com.cn>
2020-12-22 17:17:34 +0800

    first commit

---
+++ hash/goutil.go
@@ -0,0 +1,11 @@
+package hash
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("hello")
+}
+
+func Add(num1, num2 int) int {
+	return num1 + num2
+}
---
+++ hash/goutil_test.go
@@ -0,0 +1,14 @@
+package hash
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	fmt.Println(Add(1, 3))
+}
+
+func TestHello(t *testing.T) {
+	Hello()
+}
